79aa3728303364877e84e602f3bfcda6506aa925
Libor Zoubek <lzoubek@redhat.com>
2015-05-13 22:14:20 +0200

    Fix #1 Maven coordinates instead of zip location
    Plugin now accepts one more parameter called 'artifact' which is alternative
    to moduleZip. Internally it re-uses maven-dependency-plugin:copy task to
    resolve the module.zip via MojoExecutor plugin.
    
    This commit also sets default for modulesHome to
    "modules/system/layers/base", which is wildfly location.

--- src/main/java/org/wildfly/plugins/DeployExtensionMojo.java
+++ src/main/java/org/wildfly/plugins/DeployExtensionMojo.java
@@ -23,26 +23,44 @@ package org.wildfly.plugins;
 
 import java.io.File;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.BuildPluginManager;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.PluginParameterExpressionEvaluator;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
 import org.codehaus.plexus.util.FileUtils;
+
+import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
 
 /**
  * Goal which deploys JBoss module to JBoss AS7/WildFly server
  */
-@Mojo(name = "deploy", defaultPhase = LifecyclePhase.INSTALL)
+@Mojo(name = "deploy", defaultPhase = LifecyclePhase.INSTALL, requiresDependencyCollection = ResolutionScope.COMPILE)
 public class DeployExtensionMojo extends AbstractMojo {
 
 	/**
 	 * Location of JBoss input module.zip This file should have JBoss module directory structure so it can be laid down 
-	 * to modulesHome directory
+	 * to {@link #modulesHome} directory.
 	 */
 	@Parameter()
 	private File moduleZip;
+	
+	/**
+	 * An alternative to {@link #moduleZip} parameter. This parameter has higher priority than {@link #moduleZip}.
+	 * A string of the form groupId:artifactId:version[:packaging][:classifier]. Default for packaging is <strong>zip</strong> and for classifier it's <strong>module</strong>
+	 * @since 0.7
+	 */
+	@Parameter()
+	private String artifact;
 
 	/**
 	 * Location of AS7/WildFly server to deploy to
@@ -51,10 +69,10 @@ public class DeployExtensionMojo extends AbstractMojo {
 	private File jbossHome;
 	
 	/**
-	 * Location of modules home (either relative to jbossHome or absolute). Set this value unless your structure inside moduleZip does not include path to modules.
+	 * Location of modules home (either relative to {@link #jbossHome} or absolute). Set this value unless your structure inside {@link #moduleZip} does not include path to modules.
 	 * For Wildfly, this path is "modules/system/layers/base", for older AS7 versions it's just "modules".
 	 */
-	@Parameter(defaultValue = "")
+	@Parameter(defaultValue = "modules/system/layers/base")
 	private String modulesHome;
 	
 	/**
@@ -64,7 +82,7 @@ public class DeployExtensionMojo extends AbstractMojo {
 	private String serverConfig;
 	
 	/**
-	 * Location where plugin will backup original server configuration file (standalone.xml) - can be either relative to jbossHome or absolute
+	 * Location where plugin will backup original server configuration file (standalone.xml) - can be either relative to {@link #jbossHome} or absolute
 	 */
 	@Parameter(defaultValue = "standalone/configuration/standalone.xml.old")
 	private String serverConfigBackup;
@@ -90,8 +108,8 @@ public class DeployExtensionMojo extends AbstractMojo {
 	private String[] socketBindingGroups = new String[] {"standard-sockets"};
 
 	/**
-	 * List of data to be inserted to serverConfig. This is pretty powerful stuff to put/replace any XML content
-	 * anywhere in serverConfig 
+	 * List of data to be inserted to {@link #serverConfig}. This is pretty powerful stuff to put/replace any XML content
+	 * anywhere in {@link #serverConfig} 
 	 */
 	@Parameter
 	private Insert[] edit;
@@ -107,17 +125,35 @@ public class DeployExtensionMojo extends AbstractMojo {
      */
     @Parameter(defaultValue = "false")
     private boolean skipDeploy;
+    
+    @Component
+    private MavenProject mavenProject;
+
+    @Component
+    private MavenSession mavenSession;
+
+    @Component
+    private BuildPluginManager pluginManager;
+    
+    @Component
+    private MojoExecution mojoExecution;
+    
 	
 	private File serverConfigAbsolute;
 	private File serverConfigBackupAbsolute;
 	private File modulesHomeAbsolute;
 
-	public void execute() throws MojoExecutionException,MojoFailureException {
+	public void execute() throws MojoExecutionException, MojoFailureException {
 		if (skipDeploy) {
 			getLog().info("Skipped execution");
 			return;
 		}
 		validConfiguration();
+		
+		if (artifact != null) {
+			moduleZip = resolveArtifactModuleZip();
+		}
+		
 		JBossModule module = null;
 		if (moduleZip != null) {
 			try {
@@ -160,12 +196,69 @@ public class DeployExtensionMojo extends AbstractMojo {
 		}	
 	}
 	
+	private File resolveArtifactModuleZip() throws MojoExecutionException {
+		if (this.artifact == null) {
+			return null;
+		}
+		getLog().info("Invoking maven-dependency-plugin:copy to pull "+artifact);
+		
+		String gav = String.valueOf(this.artifact);
+		// there's no way to set output file name, so we need to parse artifact
+		// we now how output file will look like because we call maven-dependency-plugin with stripClassifier=true and stripVersion=true
+		final String tmpOutputDir = "${project.build.directory}/tmp/wildfly-extension-plugin";
+		String[] pieces = gav.split(":");
+		String moduleTempFile = pieces[1];
+		if (pieces.length > 3) { // packaging was specified
+			moduleTempFile += "."+pieces[3];
+		} else {
+			// fill defaults
+			moduleTempFile += ".zip";
+			gav+=":zip:module";
+		}
+		executeMojo(
+			    plugin(
+			        groupId("org.apache.maven.plugins"),
+			        artifactId("maven-dependency-plugin"),
+			        version("2.10")
+			    ),
+			    goal("copy"),
+			    configuration(
+			        element(name("outputDirectory"), tmpOutputDir),
+			        element(name("overWriteIfNewer"), "true"),
+			        element(name("overWriteReleases"), "true"),
+			        element(name("overWriteSnapshots"), "true"),
+			        element(name("stripClassifier"), "true"),
+			        element(name("stripVersion"), "true"),
+			        element(name("artifact"), gav)
+			    ),
+			    executionEnvironment(
+			        mavenProject,
+			        mavenSession,
+			        pluginManager
+			    )
+			);
+		
+		moduleTempFile = String.valueOf(evalPluginParameterExpression(tmpOutputDir+"/"+moduleTempFile));
+		return new File(moduleTempFile);
+	}
+	
+	private Object evalPluginParameterExpression(String expression) {
+		PluginParameterExpressionEvaluator evaluator = new PluginParameterExpressionEvaluator(mavenSession, mojoExecution);
+		try {
+			return evaluator.evaluate(expression);
+		} catch (ExpressionEvaluationException e) {
+			getLog().error("Failed to evaluate ["+expression+"]", e);
+		}
+		return null;
+	}
+	
 	public void register(RegisterOptions options) throws Exception {
 		File serverConfig = options.getServerConfig();
 		getLog().info("Backup original serverConfig ["+serverConfig.getAbsolutePath()+"] to ["+options.getServerConfigBackup().getAbsolutePath()+"]");
 		FileUtils.copyFile(serverConfig, options.getServerConfigBackup());		
 		new RegisterExtension(getLog()).register(options);
 	}
+	
 	
 	
 	private void validConfiguration() throws MojoFailureException {
